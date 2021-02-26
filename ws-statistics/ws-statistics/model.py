830c8e959f08205f177334070b935a818ecf6e20
Jonathan Schaeffer <jonathan.schaeffer@univ-grenoble-alpes.fr>
2021-02-26 11:24:48 +0100

    model followup

--- ws-statistics/ws-statistics/model.py
+++ ws-statistics/ws-statistics/model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, Sequence, String, Date
+from sqlalchemy import Column, Sequence, String, Date, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,6 +8,7 @@ class node(Base):
     """
     EIDA node
     """
+    __tablename__ = 'nodes'
     id = Column(Sequence, primary_key=True)
     name = Column(String(16))
 
@@ -15,16 +16,28 @@ class token(Base):
     """
     AutBasehentication token
     """
+    __tablename__ = 'tokens'
     id = Column(Sequence, primary_key=True)
+    node_id = Column(Integer, ForeignKey('nodes.id'))
 
 class payload(Base):
     """
     Payloads objects
     """
+    __tablename__ = 'payloads'
     id = Column(Sequence, primary_key=True)
+    node_id = Column(Integer, ForeignKey('nodes.id'))
 
 class dataselectstat(Base):
     """
     Aggregated dataselect statistic
     """
+    __tablename__ = 'dataselectstats'
     date = Column(Date)
+    node_id = Column(Integer, ForeignKey('nodes.id'))
+    network = Column(String(6))
+    station = Column(String(6))
+    location = Column(String(2))
+    channel = Column(String(3))
+    country = Column(String(2))
+    # TODO client is HLL object
