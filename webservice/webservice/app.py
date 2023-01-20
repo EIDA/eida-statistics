028f2b6e7db3fad7544724c7de74b9b22d1ae2de
Jonathan Schaeffer <jonathan.schaeffer@cnrs.fr>
2023-01-20 10:52:24 +0100

    fix doc path

--- webservice/webservice/app.py
+++ webservice/webservice/app.py
@@ -30,7 +30,7 @@ app.config['EIDASTATS_API_HOST'] = os.getenv('EIDASTATS_API_HOST', 'localhost:50
 app.config['EIDASTATS_API_PATH'] = os.getenv('EIDASTATS_API_PATH', '')
 
 swaggerui_blueprint = get_swaggerui_blueprint('/', '/static/openapi.yaml')
-app.register_blueprint(swaggerui_blueprint,  url_prefix=app.config['EIDASTATS_API_PATH'])
+app.register_blueprint(swaggerui_blueprint)
 
 @app.route('/dataselect/old_doc')
 def documentation():
