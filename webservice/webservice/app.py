4c603ebd38b468520a2c922b9462979ca3910b7f
Jonathan Schaeffer <jonathan.schaeffer@cnrs.fr>
2023-01-24 15:03:00 +0100

    Authentication check first

--- webservice/webservice/app.py
+++ webservice/webservice/app.py
@@ -539,6 +539,20 @@ def add_stat():
     Adding the posted statistic to the database
     """
     app.logger.info("Receiving statistics")
+
+    # Check authentication token
+    if request.headers.get('Authentication') is not None:
+        app.logger.debug("Headers: %s", request.headers.get('Authentication'))
+        try:
+            node_id = get_node_from_token(request.headers.get('Authentication').split(' ')[1])
+        except ValueError:
+            return ("No valid token provided", 403)
+        except psycopg2.Error:
+            return ("Internal error", 500)
+    else:
+        return ("No token provided. Permission denied", 401)
+
+    # Analyse payload
     if request.is_json:
         app.logger.debug("Data is JSON")
         payload = request.get_json()
@@ -551,17 +565,6 @@ def add_stat():
             app.logger.error(err)
             return("Data can not be parsed as JSON format", 400)
 
-    app.logger.debug("Headers: %s", request.headers.get('Authentication'))
-
-    if request.headers.get('Authentication') is not None:
-        try:
-            node_id = get_node_from_token(request.headers.get('Authentication').split(' ')[1])
-        except ValueError:
-            return ("No valid token provided", 403)
-        except psycopg2.Error:
-            return ("Internal error", 500)
-    else:
-        return ("No token provided. Permission denied", 401)
     if not check_payload(payload):
         return("Malformed payload", 400)
     try:
