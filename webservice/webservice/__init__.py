826a3216cf6a9ef59b489aed654c4735b189d64a
Jonathan Schaeffer <jonathan.schaeffer@cnrs.fr>
2023-02-03 10:28:48 +0100

    Try to fix the path

--- webservice/webservice/__init__.py
+++ webservice/webservice/__init__.py
@@ -6,7 +6,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_settings({'DBURI': os.getenv('DBURI', 'postgresql://postgres:password@localhost:5432/eidastats')})
     config.include("pyramid_openapi3")
-    config.pyramid_openapi3_spec(os.path.join(os.path.dirname(__file__), "openapi.yaml"))
+    config.pyramid_openapi3_spec(os.path.join(os.path.dirname(__file__), "openapi.yaml"), route='eidaws/statistics/1/')
     config.pyramid_openapi3_add_explorer(route='/', template=os.path.join(os.path.dirname(__file__),"static/index.html"))
     config.registry.settings["pyramid_openapi3.enable_request_validation"] = False
     config.add_route('health', '/_health')
