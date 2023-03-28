8946810e66cf0c564ff0ea98cb0e1391246d042f
Vasilis Petrakopoulos <vasilis4998@gmail.com>
2023-03-28 12:45:37 +0300

    Issue #32

--- webservice/ws_eidastats/views_restrictions.py
+++ webservice/ws_eidastats/views_restrictions.py
@@ -47,49 +47,28 @@ def isRestricted(request, internalCall=False, datacenter=None, network=None):
         return Response(json={"restricted": "no", "group":row.eas_group}, content_type='application/json')
 
 
-@view_config(route_name='noderestriction')
+@view_config(route_name='noderestriction', request_method='GET')
 def node_restriction_policy(request):
     """
-    Returns new or existing default restriction policy of data center
+    Returns existing default restriction policy of data center
     Returns 400 bad request if invalid request parameter given
-    Returns 401 unauthorized if authentication is unsuccessful
     """
 
     log.info(f"{request.method} {request.url}")
-    if request.method != 'POST':
-        return Response("<h1>405 Method Not Allowed</h1><p>Only POST method allowed</p>", status_code=405)
 
     # check parameters of request
     if 'datacenter' not in request.params:
         return Response(f"<h1>400 Bad Request</h1><p>'datacenter' parameter is required</p>", status_code=400)
     for key in request.params:
         log.debug('Parameter: '+key)
-        if key not in ['datacenter', 'policy']:
+        if key != 'datacenter':
             return Response(f"<h1>400 Bad Request</h1><p>Invalid parameter '{key}'</p>", status_code=400)
-        if key == 'policy' and request.params.get(key) not in ['0', '1']:
-            return Response(f"<h1>400 Bad Request</h1><p>Unsupported value for parameter '{key}'</p>", status_code=400)
 
     log.info('Checked parameters')
 
-    # check authentication
-    # if authentication successful, return dictionary with token info
-    # else return 401 unauthorized
-    try:
-        tokenDict = check_authentication(request)
-    except Exception:
-        return Response("<h1>401 Unauthorized</h1><p>Malformed token file provided</p>", status_code=401)
-    if 'Failed_message' in tokenDict:
-        return Response(f"<h1>401 Unauthorized</h1><p>{tokenDict['Failed_message']}</p>", status_code=401)
-
-    log.info('Checked authentication')
-
-    # check authorization
-    # only a datacenter operator can perform this action regarding only their own datacenter
-    memberOf = re.findall(r'/epos/(\w+)', tokenDict['memberof'])
     try:
         session = Session()
-        sqlreq = session.query(Node).with_entities(Node.restriction_policy, Node.eas_group)
-        sqlreq = sqlreq.filter(Node.name == request.params.get('datacenter'))
+        sqlreq = session.query(Node).filter(Node.name == request.params.get('datacenter'))
         session.close()
     except Exception as e:
         log.error(str(e))
@@ -97,75 +76,34 @@ def node_restriction_policy(request):
 
     row = sqlreq.first()
     if not row:
-        return Response(f"<h1>400 Bad Request</h1><p>No such datacenter</p>", status_code=400)
+        return Response(f"<h1>400 Bad Request</h1><p>No entry that matches given datacenter parameter</p>", status_code=400)
 
-    if row.eas_group not in memberOf:
-        return Response("<h1>403 Forbidden</h1><p>User has no authority to perform the requested action</p>", status_code=403)
-
-    log.info('Checked authorization')
-
-    # if policy parameter is specified, update the record in database and return the new policy
-    if 'policy' in request.params:
-        try:
-            session = Session()
-            sqlreq = session.query(Node).filter(Node.name == request.params.get('datacenter'))
-            sqlreq = sqlreq.update({'restriction_policy': int(request.params.get('policy'))})
-            session.commit()
-            session.close()
-        except Exception as e:
-            log.error(str(e))
-            return Response("<h1>500 Internal Server Error</h1><p>Database connection error</p>", status_code=500)
-
-        return Response(json={"restriction_policy": request.params.get('policy')}, content_type='application/json')
-
-    # if policy parameter not specified, return current policy from database
     return Response(json={"restriction_policy": "1" if row.restriction_policy else "0" if row.restriction_policy is not None else None},
             content_type='application/json')
 
 
-@view_config(route_name='networkrestriction')
+@view_config(route_name='networkrestriction', request_method='GET')
 def network_restriction_policy(request):
     """
-    Returns new or existing state on whether network agrees with its data center restriction policy
+    Returns existing state on whether network agrees with its data center restriction policy
     Returns 400 bad request if invalid request parameter given
-    Returns 401 unauthorized if authentication is unsuccessful
     """
 
     log.info(f"{request.method} {request.url}")
-    if request.method != 'POST':
-        return Response("<h1>405 Method Not Allowed</h1><p>Only POST method allowed</p>", status_code=405)
 
     # check parameters of request
     if any(x not in request.params for x in ['datacenter', 'network']):
         return Response(f"<h1>400 Bad Request</h1><p>Both 'datacenter' and 'network' parameters are required</p>", status_code=400)
     for key in request.params:
         log.debug('Parameter: '+key)
-        if key not in ['datacenter', 'network', 'invert_policy', 'eas_group']:
+        if key not in ['datacenter', 'network']:
             return Response(f"<h1>400 Bad Request</h1><p>Invalid parameter '{key}'</p>", status_code=400)
-        if key == 'invert_policy' and request.params.get(key) not in ['0', '1']:
-            return Response(f"<h1>400 Bad Request</h1><p>Unsupported value for parameter '{key}'</p>", status_code=400)
 
     log.info('Checked parameters')
 
-    # check authentication
-    # if authentication successful, return dictionary with token info
-    # else return 401 unauthorized
-    try:
-        tokenDict = check_authentication(request)
-    except Exception:
-        return Response("<h1>401 Unauthorized</h1><p>Malformed token file provided</p>", status_code=401)
-    if 'Failed_message' in tokenDict:
-        return Response(f"<h1>401 Unauthorized</h1><p>{tokenDict['Failed_message']}</p>", status_code=401)
-
-    log.info('Checked authentication')
-
-    # check authorization
-    # only a datacenter operator can perform this action regarding only their own datacenter
-    memberOf = re.findall(r'/epos/(\w+)', tokenDict['memberof'])
     try:
         session = Session()
-        sqlreq = session.query(Node).join(Network)
-        sqlreq = sqlreq.with_entities(Network.inverted_policy, Network.eas_group, Node.eas_group.label('node_group'), Node.id)
+        sqlreq = session.query(Node).join(Network).with_entities(Network.inverted_policy, Network.eas_group)
         sqlreq = sqlreq.filter(Node.name == request.params.get('datacenter')).filter(Network.name == request.params.get('network'))
         session.close()
     except Exception as e:
@@ -176,34 +114,5 @@ def network_restriction_policy(request):
     if not row:
         return Response(f"<h1>400 Bad Request</h1><p>No entry that matches given datacenter and network parameters</p>", status_code=400)
 
-    if row.node_group not in memberOf:
-        return Response("<h1>403 Forbidden</h1><p>User has no authority to perform the requested action</p>", status_code=403)
-
-    log.info('Checked authorization')
-
-    # if invert_policy or eas_group parameters are specified, update the record in database and return the new inverted_policy and eas_group
-    if any(x in request.params for x in ['invert_policy', 'eas_group']):
-        update_dict = {}
-        if 'invert_policy' in request.params:
-            update_dict['inverted_policy'] = int(request.params.get('invert_policy'))
-        if 'eas_group' in request.params:
-            update_dict['eas_group'] = request.params.get('eas_group')
-
-        try:
-            session = Session()
-            sqlreq = session.query(Network).filter(Network.node_id == row.id).filter(Network.name == request.params.get('network'))
-            sqlreq.update(update_dict)
-            session.commit()
-            # query database to return updated record
-            resp = sqlreq.first()
-            session.close()
-        except Exception as e:
-            log.error(str(e))
-            return Response("<h1>500 Internal Server Error</h1><p>Database connection error</p>", status_code=500)
-
-        return Response(json={"invert_policy": "1" if resp.inverted_policy else "0" if resp.inverted_policy is not None else None,
-                "eas_group": resp.eas_group}, content_type='application/json')
-
-    # if none of the invert_policy or eas_group parameters is specified, return current inverted_policy and eas_group from database
     return Response(json={"invert_policy": "1" if row.inverted_policy else "0" if row.inverted_policy is not None else None,
             "eas_group": row.eas_group}, content_type='application/json')
