77866828ea0e9c2071326e129494e3a75bf147e5
Vasilis Petrakopoulos <vasilis4998@gmail.com>
2023-04-03 13:39:28 +0300

    Update tests

--- webservice/tests/test_app.py
+++ webservice/tests/test_app.py
@@ -7,7 +7,7 @@ from ws_eidastats import main, views_main, model
 
 
 postgresql_my_proc = factories.postgresql_noproc(host="localhost", port="5432", password="password")
-postgres_with_schema = factories.postgresql('postgresql_my_proc', dbname="test", load=['./eidastats_schema.sql'])
+postgres_with_schema = factories.postgresql('postgresql_my_proc', dbname="test", load=['./tests/eidastats_schema.sql'])
 
 def test_postgres(postgres_with_schema):
     """
@@ -46,12 +46,12 @@ def test_wrong_parameter_value_date(app):
     assert 'Unsupported value for parameter' in str(response.body)
 
 
-def test_wrong_parameter_value_aggregate(app):
+def test_wrong_parameter_value_level(app):
     """
     Check request with invalid value of aggregate_on parameter given
     """
 
-    response = app.get('/dataselect/public?end=2022-01&aggregate_on=stg', status=400)
+    response = app.get('/dataselect/public?start=2022-01&level=stg', status=400)
 
     assert 'Unsupported value for parameter' in str(response.body)
 
@@ -61,7 +61,17 @@ def test_wrong_parameter_value_format(app):
     Check request with invalid value of format parameter given
     """
 
-    response = app.get('/dataselect/public?end=2022-01&format=stg', status=400)
+    response = app.get('/dataselect/public?start=2022-01&format=stg', status=400)
+
+    assert 'Unsupported value for parameter' in str(response.body)
+
+
+def test_wrong_parameter_value_details(app):
+    """
+    Check request with invalid value of format parameter given
+    """
+
+    response = app.get('/dataselect/public?start=2022-01&details=stg', status=400)
 
     assert 'Unsupported value for parameter' in str(response.body)
 
@@ -71,7 +81,7 @@ def test_correct_public_request(app):
     Check correct public request
     """
 
-    response = app.get('/dataselect/public?start=2021-05&country=GR&aggregate_on=month,station,network,datacenter', status=200)
+    response = app.get('/dataselect/public?start=2021-05&country=GR&level=network&details=month', status=200)
 
     assert 'version' in str(response.body)
 
@@ -81,7 +91,7 @@ def test_invalid_token(app):
     Check invalid token provided
     """
 
-    with open('./invalid_token', 'rb') as file:
+    with open('./tests/invalid_token', 'rb') as file:
         file_contents = file.read()
     response = app.post('/dataselect/restricted?start=2021-05&country=GR&aggregate_on=month,station,network,datacenter', content_type='application/octet-stream',
                 params=file_contents, status=401)
